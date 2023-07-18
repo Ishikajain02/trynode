540a551331052ddae3e1d7e699ea12f175aa10a2
Ishikajain02 <ishijain02@gmail.com>
2023-07-19 03:05:55 +0530

    trying node

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
+//const data = fetch('https://dummyjson.com/products');
+//use promises for that
+const products = data.products;
+const express = require ('express')
+const server = express();
+//using middleware morgan
+const morgan = require('morgan');
+server.use(morgan ('default'));
+//body parser
+server.use(express.json());
+server.use( (req,res,next)=>{
+    console.log(req.method,req.ip,req.hostname);
+    next();
+})
+   //middleware authorization
+   const auth = (req,res,next)=>{
+    console.log(req.query)
+    //three forms of sending request
+    //using body
+    //using query
+    //using params
+    if(req.body.password =='123'){
+        next();
+    }
+    else{
+        res.sendStatus(401);
+    }
+}
+server.use(auth);
+
+server.get('/', (req,res)=>{
+   // res.send('hiiii')
+    res.json(products)
+   
+})
+server.get('/', (req,res)=>{
+    res.json({type : 'GET'})
+})
+server.post('/', (req,res)=>{
+    res.json({type : 'POST'})
+})
+server.delete('/', (req,res)=>{
+    res.json({type : 'DELETE'})
+})
+
+   
+
+server.listen(8080);

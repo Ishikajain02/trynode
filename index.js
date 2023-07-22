8c153bbfa0713622ed8789084ef3369f54f1f9e8
Ishikajain02 <ishijain02@gmail.com>
2023-07-22 21:58:52 +0530

    API formation

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const products = data.products;
 const express = require ('express')
 const server = express();
 //using middleware morgan
-const morgan = require('morgan');
-server.use(morgan ('default'));
+//const morgan = require('morgan');
+//server.use(morgan ('default'));
 //body parser
 server.use(express.json());
 server.use( (req,res,next)=>{
@@ -16,26 +16,37 @@ server.use( (req,res,next)=>{
     next();
 })
    //middleware authorization
-   const auth = (req,res,next)=>{
-    console.log(req.query)
+  // const auth = (req,res,next)=>{
+    //console.log(req.query)
     //three forms of sending request
     //using body
     //using query
     //using params
-    if(req.body.password =='123'){
-        next();
-    }
-    else{
-        res.sendStatus(401);
-    }
-}
-server.use(auth);
+    //if(req.body.password =='123'){
+      //  next();
+    //}
+    //else{
+      //  res.sendStatus(401);
+    //}
+//}
+//server.use(auth);
+//products
 
-server.get('/', (req,res)=>{
+server.get('/products', (req,res)=>{
    // res.send('hiiii')
     res.json(products)
    
 })
+server.get('/products/:id',(req,res) =>{
+    const id = +req.params.id;
+    const product = products.find(p=>p.id===id)
+    res.json(product);
+});
+server.post('/products', (req,res) =>{
+    console.log(req.body);
+    products.push(req.body);
+    res.json(req.body);
+});
 server.get('/', (req,res)=>{
     res.json({type : 'GET'})
 })
